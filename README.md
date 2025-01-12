a5580f11e996fb07ab9539d12d3f602c54d0dd21
khan-milopan <khan.delagic@gmail.com>
2025-01-12 01:45:58 +0100

    Small retardation fixes + renamed the json file

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Discord Bot Token Selector
-Created by [Milopan](https://github.com/khan-milopan) with tiny tiny help from [TheGamer29](https://github.com/jacopicic).
+Created by [Milopan](https://github.com/khan-milopan) with teeny-tiny help from [TheGamer29](https://github.com/jacopicic).
 
-This is a python script that when ran reads the contents of `bot_tokens.json` containting the names and tokens of your Discord bots, after which it asks you which one you wanna choose and it returns the token of the bot that you chose.
+This Python script reads the contents of `botTokens.json` containing the names and tokens of your Discord bots, after which it asks you which one you wanna choose and it returns the token of the bot that you chose.
 
-If using Pycord, you would place the `botSelect.py` and `bot_tokens.json` into the same folder as your `main.py` (or whichever file you're running your bot from) and you'd add this the following:
+If using Pycord, you would place the `botSelect.py` and `botTokens.json` into the same folder as your `main.py` (or whichever file you're running your bot from) and you'd add the following:
 ```python
 import botSelect
 bot.run(botSelect.select())
 ```
 
-You can add or remove bots and their tokens by editing `bot_tokens.json` where you would create a new object with the key being the name of your Discord bot while the value being that bot's token.
+You can add or remove bots and their tokens by editing `botTokens.json` where you would create a new object with the key being the name of your Discord bot while the value being that bot's token.
 Example:
 ```json
 {"Bot's name": "Bot's token... blah blah blah"},
--- botSelect.py
+++ botSelect.py
@@ -1,35 +1,45 @@
 import json
 
 def select():
-    with open("bot_tokens.json", "r") as bot_tokens_file:
-        raw_tokens = json.load(bot_tokens_file)
+    try:
+        with open("botTokens.json", "r") as bot_tokens_file: # Check camelCase first
+            raw_tokens = json.load(bot_tokens_file)
+    except FileNotFoundError:
+        with open("bot_tokens.json", "r") as bot_tokens_file: # Only if no camelCase check snake_case (Because backwards compatibility blah blah)
+            raw_tokens = json.load(bot_tokens_file)
+    
     botName = [list(obj.keys())[0] for obj in raw_tokens]
     botToken = [list(obj.values())[0] for obj in raw_tokens]
+
     botList=""
     botCount=-1
     for name in botName:
         botCount += 1
-        botList += f'{name}({botCount})'
+        botList += f'   ({botCount}) "{name}"'
         if name == botName[-1]:
             botList += "?"
         else:
-            botList += ", "
+            botList += f", \n"
+    
+    error=f"\nNO BOT GOES BY THAT NUMBER!!!\n"
     while True:
         try:
-            print(f"Which bot to select: {botList}")
-            answer = int(input("(Enter the number): "))
-            if 0 <= answer < len(raw_tokens):
-                answerConfirm = input(f'You have selected "{botName[answer]}", do I select it? (Y/N) ')
+            print(f"Which bot to select:\n{botList}")
+            answerNumber = int(input("Enter the number: "))
+            if (answerNumber > -1) and (answerNumber < botCount+1):
+                answerConfirm = input(f'You have selected "{botName[answerNumber]}", is this the right choice? (Y/N) ')
                 if answerConfirm.lower() == "y":
-                    chosenToken = f"{botToken[answer]}"
+                    chosenToken = f"{botToken[answerNumber]}"
                     return chosenToken
                 elif answerConfirm.lower() == "n":
                     continue
                 else:
                     print("Invalid answer.")
                     continue
+            else:
+                print(error)
         except ValueError:
-            print("No bot goes by that number.")
+            print(error)
 
 # By Milopan (and TheGamer29/jacopicic)
 # https://github.com/khan-milopan/Discord-Bot-Token-Selector
